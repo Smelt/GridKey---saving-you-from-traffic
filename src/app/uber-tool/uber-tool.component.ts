37d515ab266879589b437c02e793ddf7a3b80901
Smelt <shivamsatyarthi@gmail.com>
2017-12-25 03:10:10 -0500

    added default pages

---
+++ src/app/uber-tool/uber-tool.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-uber-tool',
+  templateUrl: './uber-tool.component.html',
+  styleUrls: ['./uber-tool.component.css']
+})
+export class UberToolComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
