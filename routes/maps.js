4e47b433b396b49a6b134ebb1107b481fd8c1523
Smelt <shivamsatyarthi@gmail.com>
2018-02-25 16:14:44 -0500

    refactored code split routes

---
+++ routes/maps.js
@@ -0,0 +1,46 @@
+
+const   express               = require('express'),
+        router                = express.Router(),
+        timeTools             = require('../services/timeCalc.js'),
+        apiKey = "AIzaSyBjd50qu3kTyuUheWNfjIGUWBHHmH5VH0s";
+
+router.get('/mapdata/:origin/:destination', function (req, res) {
+    var origin = req.params.origin;
+    var destination = req.params.destination;
+    var googleMaps = "https://maps.googleapis.com/maps/api/directions/json?";
+    var key = "key=" + apiKey;
+    origin = 'origin=' + origin;
+    destination = 'destination=' + destination;
+    var query = googleMaps + origin + "&" + destination + "&" + key;
+  
+    request(query, function (error, response, body) {
+      if (error) {
+        handleError(error);
+      }
+      if (!error && response.statusCode == 200) {
+        var results = JSON.parse(body);
+        var duration = results.routes[0].legs[0].duration.text;
+        var seconds = results.routes[0].legs[0].duration.value;
+        var distance = results.routes[0].legs[0].distance.text;
+  
+        var dayTimes = timeTools.getWeekDayTimes(1);
+        var travelArr = timeTools.getFakeTimes(seconds, dayTimes);
+  
+        var travelObj = { arr: travelArr, distance: distance, success: true};
+        console.log("Sending response " + travelObj.success);
+       res.json(travelObj);
+      }
+  
+      else{
+        var results = JSON.parse(body);
+      }
+    });
+});
+
+function handleError(error){
+    console.log("something went wrong!");
+    console.log(error);
+}
+
+Xmodule.exports = router;
+  
---
+++ routes/uber.js
@@ -0,0 +1,12 @@
+
+const   express               = require('express'),
+        router                = express.Router();
+
+
+// Catch all other routes and return the index file
+router.get('*', (req, res) => {
+    console.log("* operator");
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+module.exports = router;
