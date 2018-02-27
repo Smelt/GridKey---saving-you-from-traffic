1d167134159c0187e4ec25d6bd5f27d48614c952
Smelt <shivamsatyarthi@gmail.com>
2018-02-26 19:23:29 -0500

    added data retrieval

--- src/app/uber-tool/uber-tool.component.ts
+++ src/app/uber-tool/uber-tool.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Headers, Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-uber-tool',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UberToolComponent implements OnInit {
 
-  constructor() { }
+  public origin: string;
+  public destination: string;
+
+  constructor(){ }
 
   ngOnInit() {
   }
+
+  public frequentLocationButton(location: string) {
+    if ( this.origin === undefined || this.origin.length === 0) {
+      this.origin = location;
+    }
+    else if (this.origin.length === 0 || this.destination === undefined || this.destination.length === 0) {
+      this.destination = location;
+    }
+
+  }
+
+  public switchButton() {
+    const tempOrigin = this.origin;
+    this.origin = this.destination;
+    this.destination = tempOrigin;
+  }
+
 
 }
