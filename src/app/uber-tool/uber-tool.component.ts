cb3b24a5a5bc60e0c65604bc5d5332cc97b722b7
Smelt <shivamsatyarthi@gmail.com>
2018-02-28 04:29:06 -0500

    ui changes and SUV

--- src/app/uber-tool/uber-tool.component.ts
+++ src/app/uber-tool/uber-tool.component.ts
@@ -15,7 +15,7 @@ export class UberToolComponent implements OnInit {
   public destination: string;
 
   public uberPrice: string = "-1";
-
+  public uberPriceXl: string = "-1";
   constructor(private uberApiService: UberApiService){ }
 
   ngOnInit() {
@@ -40,6 +40,8 @@ export class UberToolComponent implements OnInit {
   onSubmitLoc(form: NgForm) {
     const origin = form.value.origin;
     const destination = form.value.destination;
+    this.uberPrice = "24.00";
+    this.uberPriceXl = "32.00";
     this.uberApiService.getUberPrice(origin, destination).subscribe((response: Response) => {
       const uberObj = response.json();
       this.uberPrice = uberObj.price;
