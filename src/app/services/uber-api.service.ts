de180449b1ba828b26bcc5287b8aeebec5a2ae6b
Smelt <shivamsatyarthi@gmail.com>
2018-02-27 20:03:37 -0500

    process env changes in angular

--- src/app/services/uber-api.service.ts
+++ src/app/services/uber-api.service.ts
@@ -8,13 +8,13 @@ import 'rxjs/add/operator/finally';
 @Injectable()
 export class UberApiService {
 
-  private serverURL = environment.serverUrl;
+
 
   constructor(private http: Http) { }
 
    getUberPrice(origin: string, destination: string) {
      // console.log(this.serverURL + '/mapdata/' + origin + '/' + destination);
-     return this.http.get(this.serverURL + '/uber?' + 'origin=' + origin + '&destination=' + destination);
+     return this.http.get( '/uber?' + 'origin=' + origin + '&destination=' + destination);
   }
 
 }
